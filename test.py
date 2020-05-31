c7dd04f0aa9429bbf655ec897eab3888787faa52
jeremyli007 <jeremyli007@163.com>
2020-05-31 13:24:50 +0800

    增加了创建sql条件

---
+++ test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+
+url="http://ics.chinasoftinc.com/SignOnServlet"
+driver=webdriver.Chrome()
+driver.maximize_window()
+driver.get(url)
+# time.sleep(5)
+# driver.close()
+
+# driver.find_element_by_id('//input[@id="j_username"]').send_keys('174206')
+# //*[@id="dl"]/input[1]
+driver.find_element_by_xpath('//*[@id="dl"]/input[1]').send_keys('174206')
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="dl"]/div').send_keys('19931007LJ')
+driver.find_element_by_class_name('button').click()
+driver.close()
+
+
